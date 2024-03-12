6bd587e6b6599b0bd8d3953afcdf4ee69d472a55
hawa130 <handanwanga@hotmail.com>
2024-03-12 08:20:40 +0800

    style: calendar control

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -40,7 +40,7 @@ function Calendar({
         nav: 'space-x-1 flex items-center',
         nav_button: cn(
           buttonVariants({ variant: 'outline', color: 'default' }),
-          'h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100',
+          'h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 focus-visible:opacity-100',
         ),
         nav_button_previous: 'absolute left-1',
         nav_button_next: 'absolute right-1',
