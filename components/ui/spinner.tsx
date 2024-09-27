0366e28ba52e1223efcf00d659d40256115c3f22
hawa130 <handanwanga@hotmail.com>
2024-09-28 03:59:48 +0800

    feat: spinner

--- components/ui/spinner.tsx
+++ components/ui/spinner.tsx
@@ -1,11 +1,28 @@
 import { ComponentPropsWithoutRef, forwardRef } from 'react'
-import { Loader2 } from 'lucide-react'
+
 import { cn } from '@/lib/utils'
 
-export const Spinner = forwardRef<
-  SVGSVGElement,
-  ComponentPropsWithoutRef<typeof Loader2>
->(({ className, ...props }, ref) => {
-  return <Loader2 ref={ref} className={cn('animate-spin', className)} {...props} />
+export const Spinner = forwardRef<SVGSVGElement, ComponentPropsWithoutRef<'svg'>>(({ className, ...props }, ref) => {
+  return (
+    <svg
+      ref={ref}
+      width="24"
+      height="24"
+      className={cn('animate-spinner-outer origin-center', className)}
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      {...props}
+    >
+      <circle
+        className="animate-spinner-inner"
+        cx="12"
+        cy="12"
+        r="9.5"
+        fill="none"
+        strokeWidth="2.5"
+        strokeLinecap="round"
+      />
+    </svg>
+  )
 })
 Spinner.displayName = 'Spinner'
